5005a6ef62a4f0a9c1d185196cc0644751cfc997
EPICxFLIPPER <ryanfraser832@icloud.com>
2024-05-30 13:29:14 -0600

    Created a gather data function, and creaded max verstappen data

---
+++ createData.py
@@ -0,0 +1,48 @@
+import requests
+import pandas as pd
+import xmltodict, json
+
+def createURL(year,round):
+    return "http://ergast.com/api/f1/" + str(year) + "/" + str(round) + "/results.json"
+
+##Effects: Creats a pandas Dataframe for the driver with the given permanet number, and writes that dataframe to number.pkl
+def getDriverData(number,yearsBack):
+    rows = []
+    for i in range(yearsBack):
+        year = 2015 + i
+        print(year)
+        round = 1
+        while(True):
+            url = createURL(year,round)
+            response = requests.get(url)
+            data = response.json()
+            if (data["MRData"]["RaceTable"]["Races"] ==[]):
+                print("Break")
+                break
+            race_info = data["MRData"]["RaceTable"]["Races"][0]
+            raceResults = data["MRData"]["RaceTable"]["Races"][0]["Results"]
+            # Find the driver with number 33
+            driver_33_info = None
+            for result in raceResults:
+                if result["Driver"]["permanentNumber"] == str(number):
+                    driver_33_info = result
+                    break
+
+            # Create the single-row table
+            if driver_33_info:
+                single_row = {
+                    "raceName": race_info["raceName"],
+                    "date": race_info["date"],
+                    "circuitName": race_info["Circuit"]["circuitName"],
+                    "driverNumber": driver_33_info["Driver"]["permanentNumber"],
+                    "position": driver_33_info["position"]
+                }
+                rows.append(single_row)
+            round += 1
+            print("round:" + str(round))
+    df = pd.DataFrame(rows)
+    df.to_pickle(str(number)+".pkl")
+
+
+
+
