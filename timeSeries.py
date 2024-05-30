5ac96b19bf6030b59761abb2fa9be7df14822985
EPICxFLIPPER <ryanfraser832@icloud.com>
2024-05-30 17:37:50 -0600

    Created LSTM class

--- timeSeries.py
+++ timeSeries.py
@@ -4,8 +4,10 @@ import matplotlib.pyplot as plt
 from copy import deepcopy as dc
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader
 
 from TSDS import *
+
 import torch
 import torch.nn as nn
 
@@ -14,6 +16,24 @@ VERData = pd.read_pickle('Data/33.pkl')
 VERData['date'] = pd.to_datetime(VERData['date'])
 VERData = VERData[['date','position']]
 
+
+class LSTM(nn.Module):
+    def __init__(self,input_size,hidden_size,num_stacked_layers):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.num_stacked_layers = num_stacked_layers
+
+        self.lstm = nn.LSTM(input_size,hidden_size,num_stacked_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size,1)
+
+    def forward(self,x):
+        batch_size = x.size(0)
+        h0 = torch.zeros(self.num_stacked_layers, batch_size, self.hidden_size).to(device)
+        c0 = torch.zeros(self.num_stacked_layers, batch_size, self.hidden_size).to(device)
+
+        out, _ = self.lstm(x,(h0,c0))
+        out = self.fc(out[:,-1,:])
+        return out
 
 def prepData(data,steps):
     df = dc(data)
@@ -62,3 +82,6 @@ print(y_train.shape)
 
 train_data = TimeSeriesDataset(X_train,y_train)
 test_data = TimeSeriesDataset(X_test,y_test)
+
+train_loader = DataLoader(train_data,batch_size=16,shuffle=True)
+test_loader = DataLoader(test_data,batch_size=16,shuffle=False)
